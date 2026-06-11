62e3a16bccf65b3b11a762936523febeda01ef40
Footdablit2310 <kolayparamita@gmail.com>
2026-06-11 16:09:04 +0200

    Created Version 0.0.1.0-alpha

---
+++ src/main/java/com/footdablit2310/aquaaspect/AquaAspectMod.java
@@ -0,0 +1,13 @@
+package com.footdablit2310.aquaaspect;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(AquaAspectMod.MOD_ID)
+public class AquaAspectMod {
+    public static final String MOD_ID = "aquaaspect";
+
+    public AquaAspectMod() {
+        // Nothing needed.
+        // JSON enchantments load automatically from data/aquaaspect/enchantment/
+    }
+}
